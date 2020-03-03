358e25e0d97b045f5104a89bd9d50c5c82fb1a62
MacKenzie Bulkowski-Rose <mackenzie.bulkowski.rose@gmail.com>
2020-03-03 10:35:18 -0500

    Create javascript looping content

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var userInput = prompt("Type something you animal");
+
+var i;
+
+var fuckThisNonsense;
+
+for (i = 0; i < 10; i++) {
+  for (fuckThisNonsense = 0; fuckThisNonsense < i; fuckThisNonsense++) {
+    document.write(userInput)
+  }
+
+  document.write(userInput + '<br>');
+}
